8825ab127cca0d8ac43e844ad58189ff1b30c25e
Alexander Krivács Schrøder <alexschrod@gmail.com>
2025-04-26 14:35:52 +0200

    chore: remove forgotten Vuex file

--- vuex.d.ts
+++
@@ -1,9 +0,0 @@
-import { Store } from 'vuex'
-import { State } from './src/store'
-
-declare module 'vue' {
-    // provide typings for `this.$store`
-    interface ComponentCustomProperties {
-        $store: Store<State>
-    }
-}
